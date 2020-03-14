dd1bbd8d72b04a592db5b2e326fc13a85c4ed8a0
nrs-minhdoan <minh.doan.nrs@gmail.com>
2020-03-14 20:54:36 +0700

    [minhdoan] Fix react-ga

--- src/helpers/ga-setup.js
+++ src/helpers/ga-setup.js
@@ -1,7 +1,9 @@
 import ReactGA from "react-ga";
 
 function initializeReactGA() {
-  ReactGA.initialize("UA-160662152-1");
+  ReactGA.initialize("UA-160662152-1", {
+    alwaysSendToDefaultTracker: false
+  });
   ReactGA.pageview(window.location.href);
 }
 
