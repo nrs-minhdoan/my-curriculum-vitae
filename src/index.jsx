6e86975127eb12b7153a86bfaa0e7c9a0bcd7241
nrs-minhdoan <minh.doan.nrs@gmail.com>
2020-03-14 16:07:35 +0700

    [minhdoan] Fix GA

--- src/index.jsx
+++ src/index.jsx
@@ -2,9 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import * as serviceWorker from "./serviceWorker";
-import './helpers/ga-setup';
 import App from "./App";
 
+import './helpers/ga-setup';
 import "./index.css";
 
 ReactDOM.render(<App />, document.getElementById("root"));
